772673a0d015228f8a27e3875c06eee10b2bc393
Leonardo Firme <102005275+LeonardoFirm@users.noreply.github.com>
2022-12-30 19:44:45 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'u189057706_leonardo');
-define('SENHA', 'Leonardo1812@');
-define('DB', 'u189057706_login');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
